6198b8af3aeb8b9395b66b7f7611601dd09e7b0c
nayefyahya98 <75945162+nayefyahya98@users.noreply.github.com>
2021-02-21 01:03:23 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, 'cytotecpro.com');
-console.log('Server running at http://cytotecpro.com:8080/');
+}).listen(8080, '51.222.37.131');
+console.log('Server running at http://51.222.37.131:8080/');
